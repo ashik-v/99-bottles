190393ee0d8b45d7c401334365086d4e434fe6ae
Ashik Varghese <ashikv@nulogy.com>
2023-04-26 09:53:30 -0400

    Flock starting bottle count difference

--- bottles.rb
+++ bottles.rb
@@ -13,7 +13,7 @@ class Bottles
   def verse(number)
     case number
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer." + "\n" +
+      "#{number} bottles of beer on the wall, #{number} bottles of beer." + "\n" +
         "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer." + "\n" +
