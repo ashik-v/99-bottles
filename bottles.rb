11899691c4e21009c6d80e8182d3571ecd6be6d3
Ashik Varghese <ashikv@nulogy.com>
2023-04-26 09:57:16 -0400

    Have the other context supply argument

--- bottles.rb
+++ bottles.rb
@@ -14,7 +14,7 @@ class Bottles
     case number
     when 2
       "#{number} bottles of beer on the wall, #{number} bottles of beer." + "\n" +
-        "Take one down and pass it around, #{number-1} bottle of beer on the wall.\n"
+        "Take one down and pass it around, #{number-1} #{container(number - 1)} of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer." + "\n" +
         "Take it down and pass it around, no more bottles of beer on the wall.\n"
