4a53677c9eee0d9f69ba803a4554318d4ddd4c4c
Ashik Varghese <ashikv@nulogy.com>
2023-04-28 09:28:25 -0400

    Use extracted method

--- bottles.rb
+++ bottles.rb
@@ -13,13 +13,13 @@ class Bottles
   def verse(number)
     case number
     when 1
-      "#{number} #{container(number)} of beer on the wall, #{number} bottle of beer." + "\n" +
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer." + "\n" +
         "Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, no more bottles of beer." + "\n" +
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-     "#{number} bottles of beer on the wall, #{number} bottles of beer." + "\n" +
+     "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer." + "\n" +
       "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
