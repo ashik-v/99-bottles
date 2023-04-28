501b5be1740a8210ecb9a0c808ea9f29cc2b69bc
Ashik Varghese <ashikv@nulogy.com>
2023-04-27 21:03:13 -0400

    Flock the first phrase of case 1 and the default verse

--- bottles.rb
+++ bottles.rb
@@ -13,7 +13,7 @@ class Bottles
   def verse(number)
     case number
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer." + "\n" +
+      "#{number} #{container(number)} of beer on the wall, #{number} bottle of beer." + "\n" +
         "Take it down and pass it around, no more bottles of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, no more bottles of beer." + "\n" +
