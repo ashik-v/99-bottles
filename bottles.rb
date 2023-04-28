2750c7461e7b72ea50c47bd35e445bb154c602b9
Ashik Varghese <ashikv@nulogy.com>
2023-04-28 09:29:42 -0400

    Delete obsolete branch

--- bottles.rb
+++ bottles.rb
@@ -12,9 +12,6 @@ class Bottles
 
   def verse(number)
     case number
-    when 1
-      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer." + "\n" +
-        "Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, no more bottles of beer." + "\n" +
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
