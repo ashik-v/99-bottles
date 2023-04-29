ed70e34fad48897e4c670df2e74de6596fa9c563
Ashik Varghese <ashikv@nulogy.com>
2023-04-29 16:56:47 -0400

    Newspaper style

--- bottles.rb
+++ bottles.rb
@@ -17,19 +17,11 @@ class Bottles
 
   private
 
-  def successor(number)
+  def quantity(number)
     if number == 0
-      99
+      "no more"
     else
-      number - 1
-    end
-  end
-
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
+      number.to_s
     end
   end
 
@@ -41,19 +33,27 @@ class Bottles
     end
   end
 
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+
   def pronoun(number)
     if number == 1
       "it"
     else
       "one"
-    end
-  end
-
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
     end
   end
 end
