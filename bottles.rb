f69652280279a19c80051f529eef8682e2974b1c
Ashik Varghese <ashikv@nulogy.com>
2023-04-29 16:54:54 -0400

    Extract method

--- bottles.rb
+++ bottles.rb
@@ -11,17 +11,19 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer." + "\n" +
-        "#{action(number)}, 99 #{container(number)} of beer on the wall.\n"
-    else
      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer." + "\n" +
-       "#{action(number)}, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
-    end
+       "#{action(number)}, #{quantity(successor(number))} #{container(number - 1)} of beer on the wall.\n"
   end
 
   private
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
 
   def action(number)
     if number == 0
