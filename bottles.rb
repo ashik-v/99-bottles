c2f217df67ae30f696a651a6cec66270eb8e367f
Ashik Varghese <ashikv@nulogy.com>
2023-04-29 16:48:07 -0400

    Extract method

--- bottles.rb
+++ bottles.rb
@@ -14,14 +14,22 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer." + "\n" +
-        "Go to the store and buy some more, 99 #{container(number)} of beer on the wall.\n"
+        "#{action(number)}, 99 #{container(number)} of beer on the wall.\n"
     else
      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer." + "\n" +
-      "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number - 1)} of beer on the wall.\n"
+       "#{action(number)}, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
   private
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
 
   def container(number)
     if number == 1
