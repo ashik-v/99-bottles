8bd17b00b9bbbd346a78fd2e5ebad43a5b035024
Ashik Varghese <ashikv@nulogy.com>
2023-04-30 21:56:30 -0400

    Extract method

--- bottles.rb
+++ bottles.rb
@@ -14,13 +14,13 @@ class Bottles
     case number
     when 1
       "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer." + "\n" +
-        "Take #{pronoun(number)} down and pass it around, no more bottles of beer on the wall.\n"
+        "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number - 1)} of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, no more bottles of beer." + "\n" +
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer." + "\n" +
-      "Take #{pronoun(number)} down and pass it around, #{number-1} #{container(number - 1)} of beer on the wall.\n"
+      "Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -39,6 +39,14 @@ class Bottles
       "it"
     else
       "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number
     end
   end
 end
