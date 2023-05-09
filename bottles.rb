07c0069d61abe91e86a6bbc2d149d37eb0be95f8
Ashik Varghese <ashikv@nulogy.com>
2023-05-09 09:27:38 -0400

    Extract new file for extracted class

--- bottles.rb
+++ bottles.rb
@@ -1,3 +1,5 @@
+require_relative "bottle_number"
+
 class Bottles
   def song
     verses(99, 0)
@@ -35,53 +37,5 @@ class Bottles
 
   def pronoun(number)
     BottleNumber.new(number).pronoun
-  end
-end
-
-class BottleNumber
-  attr_reader :number
-
-  def initialize(number)
-    @number = number
-  end
-
-  def quantity
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
-  end
-
-  def container
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-
-  def action
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
-  end
-
-  def successor
-    if number == 0
-      99
-    else
-      number - 1
-    end
-  end
-
-  def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
   end
 end
