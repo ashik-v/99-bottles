6e61d96afece3fa573499a4a2021680898a379f2
Ashik Varghese <ashikv@nulogy.com>
2023-05-09 20:12:06 -0400

    Remove redundant methods

--- bottles.rb
+++ bottles.rb
@@ -19,26 +19,4 @@ class Bottles
      "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, #{bottle_number.quantity} #{bottle_number.container} of beer." + "\n" +
        "#{bottle_number.action}, #{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
   end
-
-  private
-
-  def quantity(number)
-    BottleNumber.new(number).quantity
-  end
-
-  def container(number)
-    BottleNumber.new(number).container
-  end
-
-  def action(number)
-    BottleNumber.new(number).action
-  end
-
-  def successor(number)
-    BottleNumber.new(number).successor
-  end
-
-  def pronoun(number)
-    BottleNumber.new(number).pronoun
-  end
 end
