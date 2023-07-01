b307336762a0f8ddfd4bdda9c860bd3e4d95af78
Ashik Varghese <ashikv@nulogy.com>
2023-07-01 10:41:54 -0600

    Implement the verse 99

--- bottles_test.rb
+++ bottles_test.rb
@@ -1,6 +1,24 @@
 gem 'rspec'
 require_relative 'bottles'
 
+# API
+# verse(n)
+# verses(a, b)
+# song
+
+describe "#verse" do
+  it "generates the last verse" do
+    expected = <<~VERSE
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+    VERSE
+
+    result = Bottles.new.verse(99)
+
+    expect(result).to eq(expected)
+  end
+end
+
 SONG = <<-SONG 
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
