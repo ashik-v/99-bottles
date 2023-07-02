298458090c10520cba20c30e8e1e573e725d2ce4
Ashik Varghese <ashikv@nulogy.com>
2023-07-02 12:43:06 -0600

    Introduce factory method

--- bottles.rb
+++ bottles.rb
@@ -2,8 +2,8 @@ require_relative "bottle_number"
 
 class Bottles
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
+    next_bottle_number = BottleNumber.for(bottle_number.successor)
 
     "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, #{bottle_number.quantity} #{bottle_number.container} of beer." + "\n" +
       "#{bottle_number.action}, #{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
