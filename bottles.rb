34028cdc10ecf4297793585c446c540904f237e6
Ashik Varghese <ashikv@nulogy.com>
2023-07-02 09:48:07 -0600

    Flock 1 and else branh

--- bottles.rb
+++ bottles.rb
@@ -5,11 +5,11 @@ class Bottles
       "No more bottles of beer on the wall, no more bottles of beer." + "\n" +
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer." + "\n" +
-        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+      "#{number} #{quantity(number)} of beer on the wall, #{number} #{quantity(number)} of beer." + "\n" +
+        "Take #{pronoun(number)} down and pass it around, #{successor(number)} #{quantity(number - 1)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer." + "\n" +
-        "Take one down and pass it around, #{number - 1} #{quantity(number - 1)} of beer on the wall.\n"
+      "#{number} #{quantity(number)} of beer on the wall, #{number} #{quantity(number)} of beer." + "\n" +
+        "Take #{pronoun(number)} down and pass it around, #{successor(number)} #{quantity(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -18,6 +18,22 @@ class Bottles
       "bottle"
     else
       "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor(number)
+    if number == 1
+      "no more"
+    else
+      number - 1
     end
   end
 
